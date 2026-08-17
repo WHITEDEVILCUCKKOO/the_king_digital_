f510eade6299217ebfd5e86a5577c50135739a07
Ajay Yadav <ajay.yadu.dev@gmail.com>
2026-08-17 15:54:17 +0530

    Added section about and fixed stats and why

--- about.php
+++ about.php
@@ -1,16 +1,20 @@
-<?php
+<?php require_once __DIR__ . '/includes/header.php'; ?>
 
-require_once __DIR__ . '/includes/header.php';
+<style>
+    .about-main {
+        font-family: 'Segoe UI', Roboto, Tahoma, Geneva, Verdana, sans-serif;
+    }
+</style>
 
-?>
-
-<main>
-
+<main class="about-main">
     <!-- section 1 ( Hero ) -->
     <?php require_once __DIR__ . '/includes/about-sections/about-hero-section.php'; ?>
     
-    <!-- section 2 ( stats ) -->
+    <!-- section 2 ( Stats ) -->
     <?php require_once __DIR__ . '/includes/about-sections/about-stats-section.php'; ?>
+
+    <!-- section 3 ( About Us ) -->
+    <?php require_once __DIR__ . '/includes/about-sections/about-section.php'; ?>
     
     <!-- section 3 ( why Choose ) -->
     <?php require_once __DIR__ . '/includes/about-sections/about-why-section.php'; ?>
@@ -19,8 +23,4 @@ require_once __DIR__ . '/includes/header.php';
     <?php require_once __DIR__ . '/includes/about-sections/about-team-section.php'; ?>
 </main>
 
-<?php
-
-require_once __DIR__ . '/includes/footer.php';
-
-?>
+<?php require_once __DIR__ . '/includes/footer.php'; ?>
