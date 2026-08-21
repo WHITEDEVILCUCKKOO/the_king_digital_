801c51a603fd2f7c1cc9b2d5650f5374b5724e8c
Ajay Yadav <ajay.yadu.dev@gmail.com>
2026-08-21 17:50:56 +0530

    Added and almost finished why section

---
+++ includes/seo-service-sections/seo-faqs.php
@@ -0,0 +1,14 @@
+<style>
+
+</style>
+
+<section class="seo-faqs">
+    <div class="seo-faqs_content">
+        <div class="seo-faqs_content--heading">
+            <h1>People Also Ask</h1>
+        </div>
+        <div class="seo-faq-container">
+            
+        </div>
+    </div>
+</section>
